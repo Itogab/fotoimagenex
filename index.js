c1b275a0d2fa02a5d972187db886415c5ed7c26d
Tomás Gabriel Ithurburu <tomasithurburu@gmail.com>
2026-04-08 14:45:20 -0300

    working forms

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import ejs from "ejs";
 import bodyParser from "body-parser";
+import nodemailer from "nodemailer";
 import { fileURLToPath } from 'node:url';
 import { dirname } from 'node:path';
 import path from "path";
@@ -10,11 +11,12 @@ const PORT = process.env.PORT || 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 
 app.use(express.static(__dirname + '/public'));
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json())
 
 app.get("/", (req,res) =>{
     res.render("index.ejs")
@@ -25,8 +27,41 @@ app.get("/views/trabajos/:jobs", (req,res) => {
     res.render(`trabajos/${jobs}.ejs`)
 });
 
-app.post("/", (req,res) => {
+app.post("/send-email", (req,res) => {
+    console.log(req.body);
     
+    const {name, phone, email, message} = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service:'gmail',
+        auth: {
+            user:process.env.GMAIL_USER,
+            pass:process.env.GMAIL_PASS
+        }
+    })
+
+    console.log(transporter)
+    const mailOptions = {
+        from: email,
+        to: process.env.GMAIL_USER,
+        subject:`Nuevo mensaje de ${name}`,
+        text:
+        `Nombre: ${name}
+         Teléfono: ${phone}
+         Email: ${email}
+         Mensaje: ${message}
+        `
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error){
+            console.error("Error occurred: ", error);
+            res.status(500).send('Error al enviar mail. Por favor intentelo mas tarde.')
+        } else{
+            console.log('Email sent: ', info.response);
+            res.send('Email sent successfully!')
+        }
+    })
 })
 
 app.listen(PORT, () => {
